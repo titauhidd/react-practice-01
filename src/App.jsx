d8b4717833f5b61050a66b0b9df7cd8544c96ddc
titauhidd <tauhidultonmoy1234@gmail.com>
2025-07-02 01:05:07 +0600

    worked on practicing passing props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import "./App.css";
 import Todo from "./Components/Todo/Todo";
 
 function App() {
+  const Todoo = [
+    { task: "Eat", time: "12:30" },
+    { task: "Work", time: "02:30" },
+    { task: "Sleep", time: "06:30" },
+  ];
   return (
     <>
-      <Todo task='Lear React' isDone={true}></Todo>
-      <Todo task='Keep Learnning' isDone={false}></Todo>
-      <Todo task='Good Job You Finished Learning' isDone={true}></Todo>
+      {/* <Todo task="Lear React" isDone={true}></Todo> */}
+      {Todoo.map((todood) => {
+        return <Todo todood={todood}></Todo>;
+      })}
+      {/* <Todo task="Keep Learnning" isDone={false}></Todo>
+      <Todo task="Good Job You Finished Learning" isDone={true}></Todo> */}
       <h1>Vite + React</h1>
     </>
   );
--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-
-const Todo = ({ task, isDone }) => {
+const Todo = ({ todood }) => {
   // console.log(task, isDone);
-  if (isDone) {
-    return <li>Finished: {task}</li>;
-  } else {
-    return <li>Work On: {task}</li>;
-  }
+  console.log(todood);
+  const { task, time } = todood;
+
+  // if (isDone) {
+  //   return <li>Finished: {task}</li>;
+  // } else {
+  //   return <li>Work On: {task}</li>;
+  // }
+
+  return (
+    //using ternary operator
+    // <li>
+    //   {isDone ? "Finishd" : "Please"}: {task}
+    // </li>
+    <div>
+      <p>Task: {task}</p>
+      <p>Time: {time}</p>
+    </div>
+  );
 };
 
 export default Todo;
